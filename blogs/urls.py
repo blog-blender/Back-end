ed3c55f3b20ca608a8d4dfed4eb4039117b54a47
11mones <mones_leader@hotmail.com>
2023-08-28 13:43:37 +0000

    3 endpoints remain

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import blogList, blogDetail,followerList,Category_list,BlogSearchView
+from .views import blogList, blogDetail,followerList,Category_list,BlogFollowersView
 
 urlpatterns = [
     path("", blogList.as_view(), name="blog_list"),
     path("<int:pk>/", blogDetail.as_view(), name="blog_detail"),
     path("followers/", followerList.as_view(), name="follower_list"),
     path("categories/", Category_list.as_view(), name="categories_list"),
+    path("userfollowing/", BlogFollowersView.as_view(), name="blog_followers"),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -13,6 +13,13 @@ from rest_framework.views import APIView
 class blogList(ListCreateAPIView):
     queryset = blog.objects.all()
     serializer_class = blogSerializer
+    def get_queryset(self):
+        owner = self.request.query_params.get('owner')
+        if owner:
+            queryset = blog.objects.filter(owner=owner)
+        else:
+            queryset = blog.objects.all()
+        return queryset
 
 
 class blogDetail(RetrieveUpdateDestroyAPIView):
@@ -21,7 +28,7 @@ class blogDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = blogSerializer
 
 class followerList(ListCreateAPIView):
-    permission_classes = (IsOwnerOrReadOnly,)
+    # permission_classes = (IsOwnerOrReadOnly,)
     queryset = Follower.objects.all()
     serializer_class = followerSerializer
 
@@ -29,5 +36,16 @@ class followerList(ListCreateAPIView):
 class Category_list(ListCreateAPIView):
     queryset = Categories.objects.all()
     serializer_class = CategoriesSerializer
+
+class BlogFollowersView(ListAPIView):
+
+    serializer_class = followerSerializer
+    def get_queryset(self):
+            user_id = self.request.query_params.get('user_id')
+            if user_id:
+                queryset = Follower.objects.filter(user_id=user_id)
+            else:
+                queryset = Follower.objects.all()
+            return queryset
 
 

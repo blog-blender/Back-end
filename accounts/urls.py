9eb31bac34f8e0877a7680d7f561b667be7d1bcc
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-09-02 19:16:48 +0000

    create user from form data req

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import UserRegistrationAPIView,usersListView,UserUpdateView
 
 
 urlpatterns = [
-    path('/register', UserRegistrationAPIView.as_view(), name='user-registration'),
+    path('/register', UserRegistrationAPIView, name='user-registration'),
     path('/users', usersListView.as_view(), name='user-list'),
     path('/update', UserUpdateView.as_view(), name='user-update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,16 +6,37 @@ from rest_framework import generics
 from accounts.models import CustomUser
 from .serializers import UserRegistrationSerializer,userSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.response import Response
+# class UserRegistrationAPIView(generics.CreateAPIView):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = UserRegistrationSerializer
+#     permission_classes = [AllowAny]
+#     def perform_create(self, serializer):
+#         user = serializer.save()
+#         user.set_password(serializer.validated_data['password'])
+#         user.save()
 
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def UserRegistrationAPIView(request):
+    query_dict_dict = {}
+    for key, values in request.data.lists():
+        if len(values) == 1:
+            query_dict_dict[key] = values[0]
+        else:
+            query_dict_dict[key] = values
 
-class UserRegistrationAPIView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegistrationSerializer
-    permission_classes = [AllowAny]
-    def perform_create(self, serializer):
-        user = serializer.save()
-        user.set_password(serializer.validated_data['password'])
-        user.save()
+    user_instance = UserRegistrationSerializer(data=query_dict_dict)
+    if user_instance.is_valid():
+        newuser = user_instance.save()
+    else:
+        errors = user_instance.errors
+        return Response(errors)
+    result_obj = CustomUser.objects.filter(id = f"{newuser.id}")
+    result_ser = userSerializer(result_obj[0]).data
+    print(result_obj[0])
+    return Response(result_ser)
 
 class usersListView(ListAPIView):
     serializer_class = userSerializer

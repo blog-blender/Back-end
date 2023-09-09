28ab8a621c39b3750a31748ab296c67eedaf2d6e
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-09-09 23:03:20 +0000

    data base image optinal

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class CustomUser(AbstractUser):
-    profile_pic = CloudinaryField("image",overwrite=True,format="jpg")
-    user_banner_pic = CloudinaryField("image",overwrite=True,format="jpg")
+    profile_pic = CloudinaryField("image",overwrite=True,format="jpg",null=True, blank=True)
+    user_banner_pic = CloudinaryField("image",overwrite=True,format="jpg",null=True, blank=True)
     def __str__(self):
         return self.username
